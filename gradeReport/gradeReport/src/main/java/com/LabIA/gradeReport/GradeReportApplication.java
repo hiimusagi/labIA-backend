3d22f645a6d6ac9eb960016e4493c9f7293d0301
Phan Kim Long <phanlong02.18@gmail.com>
2023-03-26 22:48:03 +0700

    install project

---
+++ gradeReport/gradeReport/src/main/java/com/LabIA/gradeReport/GradeReportApplication.java
@@ -0,0 +1,13 @@
+package com.LabIA.gradeReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeReportApplication.class, args);
+	}
+
+}
---
+++ gradeReport/gradeReport/src/test/java/com/LabIA/gradeReport/GradeReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LabIA.gradeReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
